46c10f6498c66f6404bf5bc1781d185444f8f7dd
Krishnaaraj <Krishnaaraj@KRISHNAARAJ-PC2>
2018-11-29 15:01:59 +0100

    Erster Entwurf

---
+++ Day1/Day1/Day1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nBitte zahlen eingeben zum Prüfen:");
+            String s = Console.ReadLine();
+            Calculator calc = new Calculator();
+            double ergebnis = calc.summUp(s);
+            Console.WriteLine("Ergebnis: {0}", ergebnis);
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+        }
+
+        // method under test 
+        public double summUp(string wert)
+        {
+            double summ = 0;
+            return summ;
+        }
+    }
+}
---
+++ Day1/Day1Test/Day1Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day1;
+
+namespace Day1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculator calc = new Calculator();
+            //Assert.Equals();
+            double ergebnis = calc.summUp("1122");
+            Assert.AreEqual(0, ergebnis);
+        }
+    }
+
+}
