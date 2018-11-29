79327f5d05007084cdbf6390b252fb48368dcfa0
Krishnaaraj <Krishnaaraj@KRISHNAARAJ-PC2>
2018-11-29 16:27:32 +0100

    Day1 finished

--- Day1/Day1Test/Day1Test.cs
+++ Day1/Day1Test/Day1Test.cs
@@ -7,13 +7,69 @@ namespace Day1Test
     [TestClass]
     public class UnitTest1
     {
+        Calculator calc = new Calculator();
+
         [TestMethod]
-        public void TestMethod1()
+        public void EinzelneZahlen()
         {
-            Calculator calc = new Calculator();
-            //Assert.Equals();
-            double ergebnis = calc.summUp("1122");
+            double ergebnis = calc.summUp("1234");
             Assert.AreEqual(0, ergebnis);
+        }
+
+        [TestMethod]
+        public void FortFolgendeZahlen()
+        {
+            double ergebnis = calc.summUp("1122");
+            Assert.AreEqual(3, ergebnis);
+        }
+
+        [TestMethod]
+        public void AlleZahlen()
+        {
+            double ergebnis = calc.summUp("1111");
+            Assert.AreEqual(4, ergebnis);
+        }
+
+        [TestMethod]
+        public void ErsteUndLetzteZahl()
+        {
+            double ergebnis = calc.summUp("91212129");
+            Assert.AreEqual(9, ergebnis);
+        }
+
+        [TestMethod]
+        public void ErsterVergleich()
+        {
+            double ergebnis = calc.compareHalf("1212");
+            Assert.AreEqual(6, ergebnis);
+        }
+
+        [TestMethod]
+        public void ZweiterVergleich()
+        {
+            double ergebnis = calc.compareHalf("1221");
+            Assert.AreEqual(0, ergebnis);
+        }
+
+        [TestMethod]
+        public void DritterVergleich()
+        {
+            double ergebnis = calc.compareHalf("123425");
+            Assert.AreEqual(4, ergebnis);
+        }
+
+        [TestMethod]
+        public void VierterVergleich()
+        {
+            double ergebnis = calc.compareHalf("123123");
+            Assert.AreEqual(12, ergebnis);
+        }
+
+        [TestMethod]
+        public void fuenfterVergleich()
+        {
+            double ergebnis = calc.compareHalf("12131415");
+            Assert.AreEqual(4, ergebnis);
         }
     }
 
