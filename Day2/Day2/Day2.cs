22c8296dedf6e147db5d513e985c971df871fc2c
Krishnaaraj <Krishnaaraj@KRISHNAARAJ-PC2>
2018-11-30 15:41:58 +0100

    Day2 finished

---
+++ Day2/Day2/Day2.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public class CheckSum
+    {
+        static void Main(string[] args)
+        {
+            List<string> intListe = new List<string>();
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
+            foreach (string line in lines)
+            {
+                intListe.Add(line);
+            }
+
+            CheckSum checker = new CheckSum();
+            double ergebnis = checker.calcDivSum(intListe);
+            Console.WriteLine("Ergebnis: {0}", ergebnis);
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+
+        }
+
+        //Convert from array of charactes to array of integers
+        private List<int> convertCharToIntArray(char[] charArray)
+        {
+            List<int> listOfInt = new List<int>();
+            charArray.ToList().ForEach(v => listOfInt.Add(int.Parse(v.ToString())));
+            return listOfInt;
+        }
+
+
+        public double getDifference(string wert)
+        {
+            double difference = 0;
+
+            List<int> intList = new List<int>();
+            string[] splits = wert.Split('\t');
+            foreach (string split in splits)
+            {
+                intList.Add(int.Parse(split));
+            }
+
+            intList.Sort();
+            int anzahl = intList.Count();
+
+            int ersteZahl = intList[0];
+            int letzteZahl = intList[anzahl - 1];
+
+            difference = letzteZahl - ersteZahl;
+
+            return difference;
+        }
+
+        public double calcSum (List<string> stringList)
+        {
+            double sum = 0;
+            int count = 0;
+
+            foreach (var el in stringList)
+            {
+                count++;
+                sum += getDifference(el);
+            }
+            Console.WriteLine("Count: {0}", count);
+            return sum;
+        }
+
+        public double evenDivision(string wert)
+        {
+            double result = 0;
+
+            List<int> intList = new List<int>();
+            string[] splits = wert.Split('\t');
+            foreach (string split in splits)
+            {
+                intList.Add(int.Parse(split));
+            }
+
+            intList.Sort();
+            int anzahl = intList.Count();
+
+            List<int> divList = new List<int>();
+
+            for (int i = anzahl-1; i >= 0; i--)
+            {
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (intList[i] % intList[j] == 0)
+                    {
+                        divList.Add(intList[i]);
+                        divList.Add(intList[j]);
+                        j = 0;
+                        i = 0;
+                    }
+                }
+            }
+
+            divList.Sort();
+            result = divList[1] / divList[0];
+
+            return result;
+        }
+
+        public double calcDivSum(List<string> stringList)
+        {
+            double sum = 0;
+            int count = 0;
+
+            foreach (var el in stringList)
+            {
+                count++;
+                sum += evenDivision(el);
+            }
+            Console.WriteLine("Count: {0}", count);
+            return sum;
+        }
+
+    }
+}
---
+++ Day2/Day2Test/Day2Test.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2;
+using System.Collections.Generic;
+
+namespace Day2Test
+{
+    [TestClass]
+    public class Day2Test
+    {
+        CheckSum checker = new CheckSum();
+
+        [TestMethod]
+        public void differenceHighLow1()
+        {
+            double ergebnis = checker.getDifference("5	1	9	5");
+            Assert.AreEqual(8, ergebnis);
+        }
+
+        [TestMethod]
+        public void differenceHighLow2()
+        {
+            double ergebnis = checker.getDifference("7	5	3");
+            Assert.AreEqual(4, ergebnis);
+        }
+
+        [TestMethod]
+        public void differenceHighLow3()
+        {
+            double ergebnis = checker.getDifference("2	4	6	8");
+            Assert.AreEqual(6, ergebnis);
+        }
+
+        [TestMethod]
+        public void calculateSum()
+        {
+            List<string> liste = new List<string>() { "5	1	9	5", "7	5	3", "2	4	6	8" };
+            double ergebnis = checker.calcSum(liste);
+            Assert.AreEqual(18, ergebnis);
+        }
+
+        [TestMethod]
+        public void evenDivision1()
+        {
+            double ergebnis = checker.evenDivision("5	9	2	8");
+            Assert.AreEqual(4, ergebnis);
+        }
+
+        [TestMethod]
+        public void evenDivision2()
+        {
+            double ergebnis = checker.evenDivision("9	4	7	3");
+            Assert.AreEqual(3, ergebnis);
+        }
+
+        [TestMethod]
+        public void evenDivision3()
+        {
+            double ergebnis = checker.evenDivision("3	8	6	5");
+            Assert.AreEqual(2, ergebnis);
+        }
+
+        [TestMethod]
+        public void calculateDivisionSum()
+        {
+            List<string> liste = new List<string>() { "5	9	2	8", "9	4	7	3", "3	8	6	5" };
+            double ergebnis = checker.calcDivSum(liste);
+            Assert.AreEqual(9, ergebnis);
+        }
+    }
+}
