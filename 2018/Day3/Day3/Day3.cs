137befac75e91cc6b2e1192e0b9fce4c16e3d0a8
Krishnaaraj <Krishnaaraj@KRISHNAARAJ-PC2>
2018-12-03 21:10:07 +0100

    Day 3 finished

---
+++ 2018/Day3/Day3/Day3.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class fabric
+    {
+        static void Main(string[] args)
+        {
+            List<string> intListe = new List<string>();
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
+            foreach (string line in lines)
+            {
+                intListe.Add(line);
+            }
+
+            fabric counter = new fabric();
+            double ergebnis = counter.findMatchSquares(intListe);
+            Console.WriteLine("Ergebnis: {0}", ergebnis);
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+        }
+
+        public double findMatchSquares(List<string> stringList)
+        {
+            double result = 0;
+            int maxZeile = 1050;
+            int maxSpalte = 1050;
+            //array2D[Zeile, Spalte]
+            int[,] array2D = new int[maxZeile, maxSpalte];
+            string possibleID = "";
+
+            foreach (string el in stringList)
+            {
+                string[] valueString = el.Split('@');
+                string[] keyValues = valueString[1].Split(':');
+                string[] stringStartPositions = keyValues[0].Split(',');
+                string[] stringSizeValues = keyValues[1].Split('x');
+                int[] startPositions = Array.ConvertAll(stringStartPositions, s => int.Parse(s));
+                int[] sizeValues = Array.ConvertAll(stringSizeValues, s => int.Parse(s));
+
+                for (int i = 1; i <= sizeValues[0]; i++)
+                {
+                    for (int j = 1; j <= sizeValues[1]; j++)
+                    {
+                        array2D[startPositions[0] + i, startPositions[1] + j]++;
+                    }
+                }
+            }
+
+            foreach (string el in stringList)
+            {
+                string[] valueString = el.Split('@');
+                string[] keyValues = valueString[1].Split(':');
+                string[] stringStartPositions = keyValues[0].Split(',');
+                string[] stringSizeValues = keyValues[1].Split('x');
+                int[] startPositions = Array.ConvertAll(stringStartPositions, s => int.Parse(s));
+                int[] sizeValues = Array.ConvertAll(stringSizeValues, s => int.Parse(s));
+                string possibleIDtmp = valueString[0];
+
+                for (int i = 1; i <= sizeValues[0]; i++)
+                {
+                    for (int j = 1; j <= sizeValues[1]; j++)
+                    {
+                        int aktuellerWert = array2D[startPositions[0] + i, startPositions[1] + j];
+                        if (aktuellerWert > 1)
+                        {
+                            possibleIDtmp = "";
+                        }
+                    }
+                }
+                var good = true;
+
+                if (possibleIDtmp != "")
+                    possibleID = possibleIDtmp;
+            }
+
+
+
+            for (int i = 0; i< maxZeile; i++)
+            {
+                for (int j = 0; j< maxSpalte; j++)
+                {
+                    if (array2D[i,j] >= 2)
+                    {
+                        result++;
+                    }
+                }
+            }
+            Console.WriteLine("PossibleID: {0}", possibleID);
+            return result;
+        }
+    }
+}
---
+++ 2018/Day3/Day3Test/Day3Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day3Test
+{
+    [TestClass]
+    public class Day3Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
